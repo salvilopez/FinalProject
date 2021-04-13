0ef0199b8c3902b107b0dfd075485c91d91bf51c
salvi <salvilopezronda@gmail.com>
2021-04-14 01:23:40 +0200

    Jwt

--- src/main/java/com/ssl/finalproject/security/JWTUtil.java
+++ src/main/java/com/ssl/finalproject/security/JWTUtil.java
@@ -15,5 +15,6 @@ public class JWTUtil {
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*10))
                 .signWith(SignatureAlgorithm.HS256,KEY).compact();
+
     }
 }

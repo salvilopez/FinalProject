f2669772cce5ea41f7b39023dbe260cfcda67e9b
Salvador Sierra <salvilopezronda@gmail.com>
2021-04-16 01:28:03 +0200

    cambios sonar Link

--- src/main/java/com/ssl/finalproject/security/SecurityConfig.java
+++ src/main/java/com/ssl/finalproject/security/SecurityConfig.java
@@ -31,7 +31,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
        http.csrf().disable().authorizeRequests().antMatchers("/**/auth").permitAll()
               .anyRequest().authenticated()
                .and().cors().disable().authorizeRequests().antMatchers("/**/auth").permitAll()
-               .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 

571cbeef4d01ce7b78aa2d7bc47d103c68a7abf3
salvi <salvilopezronda@gmail.com>
2021-04-25 12:27:12 +0200

    configure cors

--- src/main/java/com/ssl/finalproject/controller/ExpertController.java
+++ src/main/java/com/ssl/finalproject/controller/ExpertController.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://proyecto-ingenia-angular-biq7cnytj-salvilopez.vercel.app", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class ExpertController {
 
 

66652cc3177bb4d69a4277e995088d2f6f2a5bb1
Salvador Sierra <salvilopezronda@gmail.com>
2021-04-25 15:04:34 +0200

    heroku test

--- src/main/java/com/ssl/finalproject/controller/ExpertController.java
+++ src/main/java/com/ssl/finalproject/controller/ExpertController.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://proyecto-ingenia-angular-q244qy7ff-salvilopez.vercel.app", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@CrossOrigin(origins = "https://jovial-dubinsky-f4c2c4.netlify.app", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class ExpertController {
 
 

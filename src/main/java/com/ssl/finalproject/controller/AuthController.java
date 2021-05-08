32804845fe1da698a1124cd443409628472f690f
salvi <salvilopezronda@gmail.com>
2021-05-08 20:07:39 +0200

    terminar envio email

--- src/main/java/com/ssl/finalproject/controller/AuthController.java
+++ src/main/java/com/ssl/finalproject/controller/AuthController.java
@@ -15,7 +15,6 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
 import java.net.URISyntaxException;
 
 @RestController

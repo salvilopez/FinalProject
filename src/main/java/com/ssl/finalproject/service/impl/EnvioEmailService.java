40bf6b9159f6e957bc7ce848e4c967c2d9fd5e26
salvador <salvilopezronda@gmail.com>
2021-05-10 09:44:22 +0200

    cambio envio email de javamail a sparkpost

--- src/main/java/com/ssl/finalproject/service/impl/EnvioEmailService.java
+++ src/main/java/com/ssl/finalproject/service/impl/EnvioEmailService.java
@@ -9,20 +9,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EnvioEmailService {
-    @Autowired
-    private JavaMailSender mailSender;
 
     //Pasamos por parametro: destinatario, asunto y el mensaje
     public void sendEmail(String to, String subject, String content) {
-
-    /*    SimpleMailMessage email = new SimpleMailMessage();
-
-        email.setTo(to);
-        email.setSubject(subject);
-        email.setText(content);
-
-        mailSender.send(email);*/
-
 
         String API_KEY = "d6ee8bd2b437eb96f4d635fbebd9a570588aa1e2";
         Client client = new Client(API_KEY);

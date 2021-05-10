4f37b95d2c2e9c5428491aecd9fa7f12f4de4783
salvi <salvilopezronda@gmail.com>
2021-05-10 18:57:23 +0200

    terminar envio email

--- src/main/java/com/ssl/finalproject/security/SecurityConfig.java
+++ src/main/java/com/ssl/finalproject/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.ssl.finalproject.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -35,7 +36,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/auth/**").permitAll()
+        http.csrf().disable().authorizeRequests().antMatchers("/auth/**").permitAll().and()
+                .authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().authenticated().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 

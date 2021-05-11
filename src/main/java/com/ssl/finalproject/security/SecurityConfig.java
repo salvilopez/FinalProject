1a05509bbada062530ddca5fcb72e82c1b190f16
salvador <salvilopezronda@gmail.com>
2021-05-11 10:11:03 +0200

    Arreglo de Jwt

--- src/main/java/com/ssl/finalproject/security/SecurityConfig.java
+++ src/main/java/com/ssl/finalproject/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().and().cors().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll().and()
+        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll().and()
                 .authorizeRequests().antMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
